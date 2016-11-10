a916d08add50538f19f839dc06a1b2f976f71ec7
Gourab <gourab.jtv@gmail.com>
2016-11-10 15:24:01 +0530

    javaSampleCode removed

--- JavaSampleCode/src/com/jtv/oauth/JtvHandler.java
+++
@@ -1,8 +0,0 @@
-package com.jtv.oauth;
-/**
- * Created by JunctionTV on 07-11-2016.
- */
-public interface JtvHandler {
-	public boolean handleMessage(String message);
-	public boolean handleToken(String accessToken);
-}
